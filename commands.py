a509c7a6801e0f81b89256d17d7c93ee31b6b1e0
Anisha Datla <anishad@ho.elicoltd.com>
2020-02-05 12:22:58 +0530

    Multiple updates

--- commands.py
+++ commands.py
@@ -1,9 +1,53 @@
 #!/usr/bin/env python2.7
+import enum
 
-USB_HEADER = 0x00
-SEQ_ZERO   = 0x00
-CMD_FLAGS_READ  = 0xC0
-CMD_FLAGS_WRITE = 0x40
-CMD_TIVA_VER_GB = 0x02
-CMD_TIVA_VER_CB = 0x16
-CMD_TIVA_VER_OB = 28
+
+USB_HDR    = 0x00
+FLG_SEQ0   = 0x00
+FLG_LEN0   = 0x00
+FLG_READ   = 0xC0   # read ready
+FLG_WRITE  = 0x40   # write ready
+
+def Class cmd(enum.Enum):
+    PRF_CSUM = 1
+    WRT_DATA = 2
+    RED_FSZE = 3
+    RED_FDAT = 4
+    TIV_VERS = 5
+    STR_SCAN = 6
+    SCN_STAT = 7
+    STR_CONF = 8
+    NUM_CONF = 9
+    GET_SCON = 10
+    SET_SCON = 11
+    GET_TDAT = 12
+    DEV_STAT = 13
+
+
+
+################################################################################
+#Command Bytes :
+#[name,usb_hdr, flags:r/w, seq_no,length,Command_byte,group_byte,output_bytes]
+#
+# * for variable size output_bytes value of 0 is assigned and later changed to
+#   actual value.
+# * for reads the return data size is output_bytes + 4.
+# * for writes the return data size is output bytes + 1.
+################################################################################
+
+
+CMD_PRF_CSUM = [cmd.PRF_CSUM, USB_HDR, FLG_READ,  FLG_SEQ0, 0x02, 0x00, 0x15, 0x00, 4+4]        # Perform file checksum
+CMD_WRT_DATA = [cmd.WRT_DATA, USB_HDR, FLG_WRITE, FLG_SEQ0, 0x02, 0x00, 0x25, 0x00, 0+1]        # Write file data
+CMD_RED_FSZE = [cmd.RED_FSZE, USB_HDR, FLG_READ,  FLG_SEQ0, 0x03, 0x00, 0x2D, 0x00, 4+4]        # Read file size
+CMD_RED_FDAT = [cmd.RED_FDAT, USB_HDR, FLG_READ,  FLG_SEQ0, 0x02, 0x00, 0x2E, 0x00, 0+4]        # Read file data
+CMD_TIV_VERS = [cmd.TIV_VERS, USB_HDR, FLG_READ,  FLG_SEQ0, 0x02, 0x00, 0x16, 0x02,28+4]        # Read version info
+
+CMD_STR_SCAN = [cmd.STR_SCAN, USB_HDR, FLG_WRITE, FLG_SEQ0, 0x03, 0x00, 0x18, 0x02, 0+1]        # Start scan
+CMD_SCN_STAT = [cmd.SCN_STAT, USB_HDR, FLG_READ,  FLG_SEQ0, 0x02, 0x00, 0x19, 0x02, 1+4]        # Scan status-> 0:in progress
+                                                                                            #               1:complete
+CMD_STR_CONF = [cmd.STR_CONF, USB_HDR, FLG_READ,  FLG_SEQ0, 0x04, 0x00, 0x20, 0x02, 0+4]        # Read stored scan configs
+CMD_NUM_CONF = [cmd.NUM_CONF, USB_HDR, FLG_READ,  FLG_SEQ0, 0x02, 0x00, 0x22, 0x02, 1+4]        # Read num of stored configs
+CMD_GET_SCON = [cmd.GET_SCON, USB_HDR, FLG_READ,  FLG_SEQ0, 0x02, 0x00, 0x23, 0x02, 1+4]        # Read active scan config
+CMD_SET_SCON = [cmd.SET_SCON, USB_HDR, FLG_WRITE, FLG_SEQ0, 0x03, 0x00, 0x24, 0x02, 0+1]        # Set Active scan config
+CMD_GET_TDAT = [cmd.GET_TDAT, USB_HDR, FLG_READ,  FLG_SEQ0, 0x02, 0x00, 0x0C, 0x03, 7+4]        # Get time and date
+CMD_DEV_STAT = [cmd.DEV_STAT, USB_HDR, FLG_READ,  FLG_SEQ0, 0x02, 0x00, 0x03, 0x04, 4+4]        # Read device status
--- usb_comm.py
+++ usb_comm.py
@@ -8,30 +8,20 @@ from commands import *
 
 VID = 0x0451
 PID = 0x4200
-
 TIMEOUT = 1000
 
-scan_data_size = 0
+def setup(VID,PID):
+    device_list = hid.enumerate(VID,PID)
+    print (device_list)
+    global  h = hid.Device(VID,PID)
+    print("Product:  " + h.product +" Device:  " + h.manufacturer)
+    print("Serial Number:  " + str(h.serial))
 
-data = [ ]
-ver_cmd  =      [USB_HEADER,CMD_FLAGS_READ,SEQ_ZERO,0x02,0x00,CMD_TIVA_VER_CB,CMD_TIVA_VER_GB]
-dat_cmd  =      [USB_HEADER,CMD_FLAGS_READ,SEQ_ZERO,0x02,0x00,0x0C,0x03]
-ser_cmd  =      [USB_HEADER,CMD_FLAGS_READ,SEQ_ZERO,0x02,0x00,0x33,0x02]
-led_test_start =[USB_HEADER,CMD_FLAGS_WRITE,SEQ_ZERO,0x03,0x00,0x0B,0x01,0x01]
-led_test_stop  =[USB_HEADER,CMD_FLAGS_WRITE,SEQ_ZERO,0x03,0x00,0x0B,0x01,0x00]
-start_scan     =[USB_HEADER,CMD_FLAGS_WRITE,SEQ_ZERO,0x03,0x00,0x18,0x02,0x00]
-device_status  =[USB_HEADER,CMD_FLAGS_READ,SEQ_ZERO,0x02,0x00,0x03,0x04] 
-read_file_size =[USB_HEADER,CMD_FLAGS_READ,SEQ_ZERO,0x03,0x00,0x2D,0x00,0x00]
-read_scan_data =[USB_HEADER,CMD_FLAGS_READ,SEQ_ZERO,0x02,0x00,0x2E,0x00]
 
-device_list = hid.enumerate(VID,PID)
-print (device_list)
-
-h = hid.Device(VID,PID)
-
-def send_info(cmd,ret_len):
+def send_info(cmd_name,cmd,ret_len):
      h.write(''.join(map(chr,cmd)))
-     process_data(ret_len)
+     data = h.read(ret_len)
+     process_data(data)
 
 def extract_flags(flag_byte):
     err = (ord(flag_byte) & 0x30) >> 4
@@ -42,68 +32,25 @@ def extract_flags(flag_byte):
           " Err: " + str(err) )
     return rw
 
-def process_data(ret_len):
-     length = 0
-     read_len = ret_len
-     burst = False
+def process_data(data):
+    rw = extract_flags(data[0])
 
-     while (read_len != 0):
-         print("READ_LEN: " + str(read_len))
-         if read_len > 64:
-             read_len -= 64
-             data = h.read(64)
-         else:
-             data = h.read(read_len)
-             read_len -= read_len
-         print("\nData len: " + str(len(data)))
-         for i in range(len(data)):
-             if (i !=0 and rw == 0) or burst == True:         
-                 print(hex(ord(data[i])))
-             elif i == 0:
-                 rw = extract_flags(data[i])
-             else:                             # read  transaction
-                 if i == 1:
-    	             seq_no = ord(data[i])
-                     print("Seq_no: " + str(seq_no))
-                 elif i == 2:
-                     length += ord(data[i])
-                 elif i == 3:
-                     length += ord(data[i]) << 8
-                     print("Packet_Len: " + str(length))
-                     if len(data) != ret_len:
-                         burst = True
-                 else:
-                     print(hex(ord(data[i]))) 
-	
-
-
-print("Product:  " + h.product +" Device:  " + h.manufacturer)
-print("Serial Number:  " + str(h.serial))
-    
+    if rw == 0 and len(data) > 1:
+        print(hex(ord(data)))
+    else:                      # read  transaction
+        seq_no = ord(data[1])
+        print("Seq_no: " + str(seq_no))
+        length = ord(data[2]) + (ord(data[3]) << 8)
+        print("Packet_Len: " + str(length))
+        read_data_process(data[4:])
 
 
 
 
 
-send_info(led_test_start,2)                       # start led test  
-#send_info(ver_cmd,CMD_TIVA_VER_OB+4)             # get version no
-#print("\n") 
-#send_info(dat_cmd,11)                            # get date
-#print("\n") 
-#send_info(ser_cmd,12)	                          # get serial no
-#print("\n") 
-#time.sleep(5)                                    # 10 sec delay
 
-print ("Test #2\n")
-send_info(led_test_stop,2)                        # stop led test
 
-print ("Test #3\n")
-send_info(start_scan,1)
-time.sleep(5)
-print ("Test #4\n")
-send_info(device_status,8)
-print ("Test #5\n")
-send_info(read_file_size,8)
-print("Test #6\n")
-send_info(read_scan_data,512)
 
+setup(VID,PID)
+
+send_info (CMD_PRF_CSUM[0], CMD_PRF_CSUM[1:7], CMD_PRF_CSUM[8])
